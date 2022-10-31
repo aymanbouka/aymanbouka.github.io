09e8966ab938aec5f0ad1ff1c0237e01ee7911d8
Your Name <aymanboukharraz@icloud.com>
2022-10-30 22:21:23 -0400

    updates
    updates

---
+++ itis3135/hobby/scripts_for_midterm/button.js
@@ -0,0 +1,84 @@
+
+function ShowAndHide() 
+{
+    var x = document.getElementById('SectionName');
+
+    if(x.style.display == 'none')
+    {
+        x.style.display = 'block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideForHow() 
+{
+    var x = document.getElementById('SectionNameForHow');
+
+    if(x.style.display == 'none')
+    {
+        x.style.display = 'block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideForWhy() 
+{
+    var x = document.getElementById('SectionNameForWhy');
+
+    if(x.style.display == 'none')
+    {
+        x.style.display = 'block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideForWhen() 
+{
+    var x = document.getElementById('SectionNameForWhen');
+
+    if(x.style.display == 'none')
+    {
+        x.style.display = 'block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideForAboutMe() 
+{
+    var x = document.getElementById('SectionNameForAbout');
+
+    if(x.style.display == 'none')
+    {
+        x.style.display = 'block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideForWhere() 
+{
+    var x = document.getElementById('SectionNameForWhere');
+
+    if(x.style.display == 'none')
+    {
+        x.style.display = 'block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,19 @@
+function scriptTest()
+{
+    alert("Hey my script is running");
+}
+
+function getDate()
+{
+    
+//Displays the time, day of week, and date (i.e. Today is 3:53pm on Wednesday, 3 November, 2027)
+
+let dateAndTime = new Date().getFullYear();
+
+let time = now.getHours() % 12 || 12;
+
+console.log(dateAndTime);
+
+alert("Today is " + "")
+
+}
